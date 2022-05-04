2f12521d4e93286062352d4f189741176c1a3e8d
acan <abans@qq.com>
2022-05-04 16:24:55 +0800

    up draw

--- src/index.js
+++ src/index.js
@@ -1,28 +1,29 @@
 /*eslint-disable */
 const App = () => {
-  const excalidrawWrapperRef = React.useRef(null);
+  window.drawRef = React.useRef(null);
+  const excalidrawRef = React.useRef(null);
   const [dimensions, setDimensions] = React.useState({width: undefined, height: undefined})
   const [viewModeEnabled, setViewModeEnabled] = React.useState(false);
   const [zenModeEnabled, setZenModeEnabled] = React.useState(false);
   const [gridModeEnabled, setGridModeEnabled] = React.useState(false);
 
   React.useEffect(() => {
-    const { width, height } = excalidrawWrapperRef.current.getBoundingClientRect()
+    const { width, height } = excalidrawRef.current.getBoundingClientRect()
     setDimensions({ width, height });
     const onResize = () => {
-      const { width, height } = excalidrawWrapperRef.current.getBoundingClientRect()
+      const { width, height } = excalidrawRef.current.getBoundingClientRect()
       setDimensions({ width, height });
     };
     window.addEventListener("resize", onResize);
     return () => window.removeEventListener("resize", onResize);
-  }, [excalidrawWrapperRef]);
-
+  }, [excalidrawRef]);
   return React.createElement(React.Fragment, null,
     React.createElement("div", {
       className: "excalidraw-wrapper",
-      ref: excalidrawWrapperRef
+      ref: excalidrawRef
     }, React.createElement(Excalidraw.default,{
-      initialData: InitialData,
+      ref: drawRef,
+      initialData,
       // onChange: (elements, state) => console.log("Elements :", elements, "State : ", state),
       // onPointerUpdate: (payload) => console.log(payload),
       onCollabButtonClick: () => window.alert("You clicked on collab button"),
@@ -37,9 +38,14 @@ const main = async function() {
   const ubj = new URL(location.href)
   const src = ubj.searchParams.get('src')
   if (src) {
-    window.InitialData = await fetch(src, {json:true}).then(r=>r.json())
-    console.log(Excalidraw.exportToSvg(InitialData));
-  } else window.InitialData = {}
+    window.initialData = await fetch(src, {json:true}).then(r=>r.json())
+    // console.log(Excalidraw.exportToSvg(initialData));
+  } else window.initialData = {}
   ReactDOM.render(React.createElement(App), document.getElementById("app"));
 }
 main()
+
+window.addEventListener('message', async function(e) {
+  const rs = await fetch(e.data, {json:true}).then(r=>r.json())
+  drawRef.current.updateScene(rs)
+});
